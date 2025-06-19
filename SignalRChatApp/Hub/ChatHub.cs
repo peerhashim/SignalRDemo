d7ff215d019e33bab22d91d7fff23e43e33316d6
Peer Hashim <137474298+peerhashim@users.noreply.github.com>
2025-06-19 22:51:55 +0530

    Add files via upload

---
+++ SignalRChatApp/Hub/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            Console.WriteLine($"Message received from {user}: {message}");
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
+
